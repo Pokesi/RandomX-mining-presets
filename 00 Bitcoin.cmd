236f0994a9e7ccd2a502e57898f779fa82248d2d
Pokesi <81540683+Pokesi@users.noreply.github.com>
2021-05-11 18:27:40 +1000

    Update 00 Bitcoin.cmd

--- 00 Bitcoin.cmd
+++ 00 Bitcoin.cmd
@@ -1,3 +1,3 @@
 cd %~dp0
-xmrig.exe -o rx.unminable.com:3333 -a rx -k -u BTC:bc1qp9y98996l9dkdqcnge449x40dfh7stneklq2js.worker1 -p cmdminer -t 2 --tls --donate-level 1
+xmrig.exe -o rx.unminable.com:3333 -a rx -k -u BTC:bc1qp9y98996l9dkdqcnge449x40dfh7stneklq2js.worker1 -p cmdminer -t 2 -p x --donate-level 1
 pause
