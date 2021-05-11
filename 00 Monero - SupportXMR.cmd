79c59569f3e0e1e1e51846429a2a6e1c23276a44
Pokesi <81540683+Pokesi@users.noreply.github.com>
2021-05-11 11:41:54 +1000

    Update 00 Monero - SupportXMR.cmd

--- 00 Monero - SupportXMR.cmd
+++ 00 Monero - SupportXMR.cmd
@@ -1,3 +1,3 @@
 cd %~dp0
-xmrig.exe -o pool.supportxmr.com:9000 -a rx/0 -u 87Jr3LTEH4D7ubaxJNsrKTQPzfitSPH4cgp254Yb3cyGHLdrP6fpaNjCjdph2cn8Sh9qjgxe9UMjoCrTDbnbqJUtJXK9bb4 -p featherwallet --no-color -t 2 --tls --donate-level 1
+xmrig.exe -o pool.supportxmr.com:9000 -a rx/0 -u 87Jr3LTEH4D7ubaxJNsrKTQPzfitSPH4cgp254Yb3cyGHLdrP6fpaNjCjdph2cn8Sh9qjgxe9UMjoCrTDbnbqJUtJXK9bb4 -p cmdminer --no-color -t 2 --tls --donate-level 1
 pause
